ae7ad8f995ee044aeeae4418f17b22ddafe6d0ef
Sagar <sagarpatel1551@icloud.com>
2023-03-09 21:21:59 -0500

    my commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">HomeScreen</div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmFyhYez9YE5kQcCt1TUs-dGQ9Mmx6T_o",
+    authDomain: "netflix-45e0d.firebaseapp.com",
+    projectId: "netflix-45e0d",
+    storageBucket: "netflix-45e0d.appspot.com",
+    messagingSenderId: "189944988847",
+    appId: "1:189944988847:web:269f1eb6556c3b96e1805a"
+  };
