5045346c98b81ada85a62de468a550187a1c77f6
Sagar <sagarpatel1551@icloud.com>
2023-03-10 19:23:20 -0500

    Implemented the banner to recommend a movie

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
-    return (
-        <div className='banner'>
 
-        </div>
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+    }
+
+    const [movie, setMovie] = useState([]);
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    }, [])
+    console.log(movie)
+    return (
+        <header className='banner' style={{
+            backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center"
+        }}>
+            <div className="banner_contents">
+                <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+            </div>
+            <div className="banner--fadeBottom" />    
+        </header>
         
     )
 }
