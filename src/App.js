922b0d2009dab287b2a47945f6f7d7b8df183306
Sagar <sagarpatel1551@icloud.com>
2023-03-13 18:38:40 -0400

    Added user auth capability and displaying proper routes based on loged in state and url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen />,
+  },
+  
+]);
+
+const loginRouter = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginScreen />,
+  },
+  
+]);
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  
+  useEffect(() =>{
+    const unsubscribe = auth.onAuthStateChanged(
+      (userAuth) => {
+        if (userAuth){
+          //logged in
+          dispatch(
+            login({
+              uid: userAuth.uid,
+              email: userAuth.email
+            })
+          );
+        } else {
+          dispatch(logout);
+          //logged out
+        }
+    });
+    return unsubscribe;
+  }, []);
+
   return ( 
     <div className="app">
-      <HomeScreen />
+    <React.StrictMode>
+      {!user ? (<RouterProvider router={loginRouter}/>): (<RouterProvider router={router} />)}
+    </React.StrictMode>
     </div>
   );
 }
