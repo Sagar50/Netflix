4f7a10def87e68c73dad6db660397510aecb0780
Sagar <sagarpatel1551@icloud.com>
2023-03-13 18:37:22 -0400

    Added firebase auth capability

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAmFyhYez9YE5kQcCt1TUs-dGQ9Mmx6T_o",
     authDomain: "netflix-45e0d.firebaseapp.com",
@@ -6,3 +11,12 @@ const firebaseConfig = {
     messagingSenderId: "189944988847",
     appId: "1:189944988847:web:269f1eb6556c3b96e1805a"
   };
+
+// Use this to initialize the firebase App
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  // Use these for db & auth
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth, db };
