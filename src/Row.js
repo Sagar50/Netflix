cf0981f646caf48c12885aa553caee0e8d30aab6
Sagar <sagarpatel1551@icloud.com>
2023-03-17 16:14:14 -0400

    Added the ability to click on a movie/show img and navigate to the Show Movie page/screen

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState} from 'react'
 import './Row.css';
 import axios from './axios'
-
+import { useNavigate } from 'react-router-dom';
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
     const [movies, setMovies] = useState([]);
+    const navigate = useNavigate();
 
     const base_url = "https://image.tmdb.org/t/p/original/"
 
@@ -24,13 +25,15 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
         <h2>{title}</h2>
         <div className="row_posters">
             {movies.map((movie) => (
-                ((isLargeRow && movie.poster_path) || 
-                (!isLargeRow && movie.backdrop_path)) && (
+                ((isLargeRow && movie?.poster_path) || 
+                (!isLargeRow && movie?.backdrop_path)) && (
                 <img 
                     className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                     key={movie.id} 
                     src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                     alt={movie.name}
+                    onClick={() => navigate('/showMovie',{state:{movieInfo: movie}})}
+                    
                 />)
             ))}
         </div>
